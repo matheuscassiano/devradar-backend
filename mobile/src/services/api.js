c37e1539e0861f3d46612937c27099fe0ec46bec
Matheus Cassiano <matheuscassiano9@gmail.com>
2020-01-24 05:49:53 +0000

    build web

--- mobile/src/services/api.js
+++
@@ -1,7 +0,0 @@
-import axios from 'axios';
-
-const api = axios.create({
-	baseURL: 'https://3333-fa908d3f-ac36-4001-a791-8651d3b82c00.ws-us02.gitpod.io/',
-});
-
-export default api;
---
+++ precache-manifest.2c32b09ee2bbb5b79e7e93d1599258f6.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "03261fac90c289dc0bfc9b1da27b1d6d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "51c265f31f4d282f916c",
+    "url": "/static/css/main.21dbb4e6.chunk.css"
+  },
+  {
+    "revision": "ba594209e8d3b2d2e2ba",
+    "url": "/static/js/2.b4de15d2.chunk.js"
+  },
+  {
+    "revision": "51c265f31f4d282f916c",
+    "url": "/static/js/main.196d328b.chunk.js"
+  },
+  {
+    "revision": "2da5a306b8d324f436a0",
+    "url": "/static/js/runtime-main.7cde109d.js"
+  }
+]);
