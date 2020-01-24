25aa7c0515ee760f7d0b1d9be48aaa5a86f82b64
Matheus Cassiano <matheuscassiano9@gmail.com>
2020-01-24 06:34:11 +0000

    Updates

---
+++ precache-manifest.43000bb1f0bf015ce2e610e8623d1dec.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4582789e58c6bcd48256b424d1f7806b",
+    "url": "/devradar/index.html"
+  },
+  {
+    "revision": "e8468d2154eaee5d0170",
+    "url": "/devradar/static/css/main.21dbb4e6.chunk.css"
+  },
+  {
+    "revision": "bcf697b1bfd687e5825e",
+    "url": "/devradar/static/js/2.3e331b21.chunk.js"
+  },
+  {
+    "revision": "e8468d2154eaee5d0170",
+    "url": "/devradar/static/js/main.c2f24214.chunk.js"
+  },
+  {
+    "revision": "e9122e464e3d27df67f4",
+    "url": "/devradar/static/js/runtime-main.352972f3.js"
+  }
+]);
